831d3a7caf9e23308bd1c1d8100727250c0618ff
Ashish <ashishguptaitwa@gmail.com>
2024-12-09 11:39:09 +0530

    ///

--- js/custom.js
+++ js/custom.js
@@ -437,3 +437,25 @@ jsFileLocation:"revolution/js/",
 	
   			
     });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Get form data
+    const formData = new FormData(form);
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const subject = formData.get("subject");
+    // const address = formData.get("address");
+    const message = formData.get("message");
+
+    // Construct WhatsApp link with form data
+    const whatsappLink = `https://wa.me/9236858847?text=Name: ${name}%0ASubject: ${subject}%0AEmail: ${email}%0AMessage: ${message}`;
+
+    // Open WhatsApp link in a new tab/window
+    window.open(whatsappLink, "_blank");
+  });
+});	
